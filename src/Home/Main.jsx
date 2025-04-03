92fd0bdab6b0ccc92049499ccf008d56cbf80c48
Ritun Chakraborty <mithynead@gmail.com>
2025-04-03 12:26:50 +0530

    5th commit

--- src/Home/Main.jsx
+++ src/Home/Main.jsx
@@ -70,6 +70,8 @@ const Main = () => {
                                 }
 
 
+                        } else {
+                                console.log("Cookie is not Available")
                         }
                 }
 
